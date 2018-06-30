ade90514ed91e8da267f2125745075c41102880f
arale8220 <36830946+arale8220@users.noreply.github.com>
2018-07-01 02:26:59 +0900

    BottomNav is done
    activity can change well!!

---
+++ Contact/app/src/main/java/com/example/q/contactpractice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.q.contactpractice;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_contact;
+    }
+
+}
---
+++ Contact/app/src/main/java/com/example/q/contactpractice/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.q.contactpractice;
+
+public class MainActivity2 extends BaseActivity {
+
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_photo;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_photos;
+    }
+
+}
